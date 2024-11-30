d655eb16f9a22e61d435b61847ed2ae59f2b956c
Anatoliy Pylypchuk <anatoliy.pylypchuk@kindgeek.com>
2024-11-30 22:54:06 +0200

    Add Auth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+import { signIn, signOut } from "@/auth";
+import Hello from "@/components/hello";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <p className={styles.hello}>
-      Hello world!
-    </p>
+    <div className={styles.mainWrapper}>
+      <div className={styles.formWrapper}>
+        <form
+          action={async () => {
+            "use server"
+            await signIn("keycloak")
+          }}
+        >
+          <button type="submit" className={styles.button}>Sign In with Keycloak</button>
+        </form>
+        <form
+          action={async () => {
+            "use server"
+            await signOut()
+          }}
+        >
+          <button type="submit" className={styles.button}>Sign Out</button>
+        </form>
+      </div>
+      <Hello />
+    </div>
   );
 }
